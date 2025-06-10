c2c9dca5079c766f84e0af3398ae384c25a10208
lyweze <borodulindenis60@gmail.com>
2025-06-10 22:37:29 +0300

    убрал подключение к scaledrone, поработал с конфигом vite

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,9 +6,5 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<App />
-		<script
-			type="text/javascript"
-			src="https://cdn.scaledrone.com/scaledrone.min.js"
-		/>
 	</React.StrictMode>
 );
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,16 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	server: {
+		port: 3000,
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+				changeOrigin: true,
+				secure: false,
+			},
+		},
+	},
+	root: "./",
 	base: "/react-messenger/",
 });
